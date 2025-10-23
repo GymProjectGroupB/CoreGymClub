2bd1d5bb8b23f5e04ce2b50432a93f3e4b0e107b
Anton Såndberg <saandberg.anton@gmail.com>
2025-10-23 10:00:31 +0200

    add trainingsession display page without styling

---
+++ CoreGymClub.Presentation/ViewModels/TrainingSessionViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreGymClub.Presentation.ViewModels;
+
+public sealed record TrainingSessionViewModel(
+    int Id,
+    string SessionName,
+    DateOnly Date,
+    TimeOnly StartTime,
+    TimeOnly EndTime,
+    string Location,
+    string Instructor
+);
