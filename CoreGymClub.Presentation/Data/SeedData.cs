a0bbcf434ef0b7ae5d94c7556ac7ce2e2571f8c4
Ludwig Andersson <Ludwig.andersson94@hotmail.com>
2025-10-23 15:18:28 +0200

    Databas Migration, Training Class skapad, Roles skapad, Admin Inlogging Seedad

---
+++ CoreGymClub.Presentation/Data/SeedData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CoreGymClub.Presentation.Data
+{
+    public static class SeedData
+    {
+        public static async Task InitializeAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            // Skapa roller
+            string[] roleNames = { "Admin", "Member", "Trainer" };
+            foreach (var roleName in roleNames)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            
+            var adminEmail = "admin@hotmail.com";
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+            if (adminUser == null)
+            {
+                adminUser = new IdentityUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(adminUser, "Admin123!"); 
+                await userManager.AddToRoleAsync(adminUser, "Admin");
+            }
+        }
+    }
+}
--- CoreGymClub.Presentation/Program.cs
+++ CoreGymClub.Presentation/Program.cs
@@ -6,21 +6,36 @@ namespace CoreGymClub.Presentation
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                                   ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            // ? Ändring här: AddIdentity istället för AddDefaultIdentity
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders()
+            .AddDefaultUI();
+
             builder.Services.AddRazorPages();
 
             var app = builder.Build();
+
+            // Seed roller och admin
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                await SeedData.InitializeAsync(services);
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -30,19 +45,16 @@ namespace CoreGymClub.Presentation
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapStaticAssets();
-            app.MapRazorPages()
-               .WithStaticAssets();
+            app.MapRazorPages().WithStaticAssets();
 
             app.Run();
         }
