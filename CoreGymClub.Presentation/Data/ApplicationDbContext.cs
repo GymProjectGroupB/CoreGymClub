7ce55c1c19730cd2bcb4bcf62212ba41548e724e
Anton Såndberg <saandberg.anton@gmail.com>
2025-10-27 09:43:16 +0100

    add TrainingSessions table in DB and migrations

--- CoreGymClub.Presentation/Data/ApplicationDbContext.cs
+++ CoreGymClub.Presentation/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CoreGymClub.Presentation.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CoreGymClub.Presentation.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<TrainingSession> TrainingSessions { get; set; } = default!;
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
